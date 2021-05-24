97a35beeee1b67db7ae44a8c8dab019c70ef98b2
alvinoalvin <alvinoalvin@gmail.com>
2021-05-24 15:41:05 -0700

    player.rb

---
+++ player.rb
@@ -0,0 +1,16 @@
+class player()
+  @name;
+  @points;
+  @isWinner;
+
+  def initialize()
+   @name = name;
+   @points = 0;
+   @isWinner = false;
+  end
+  
+  def addPoints()
+    @points++;
+  end
+end
+
